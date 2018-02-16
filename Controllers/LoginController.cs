732b15ff9fdcca36e7846d0c519e6affa9450ff9
lemonde-L <35504551+lemonde-L@users.noreply.github.com>
2018-02-16 14:17:49 -0800

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        private CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(
+            [Bind(Include = 
+            "UserName, Password, PersonKey")
+            ]Login lg)
+        {
+
+            lg.PersonKey = 0;
+            int result = db.usp_Login(lg.UserName, lg.Password);
+
+            Message m = new Message();
+
+            if (result == -1)
+            {
+                m.MessageText = "Validation failed, please try again or register.";
+            }
+            else
+            {
+                var ukey = (from p in db.People
+                            where p.PersonEmail.Equals(lg.UserName)
+                            select p.PersonKey).FirstOrDefault();
+                lg.PersonKey = (int)ukey;
+                m.MessageText = "Thank you for logging in. You are now free to donate or apply for assistance";
+            }
+
+            return View("LoginResult", m);
+        }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int PersonKey { get; set; }
+    }
+}
